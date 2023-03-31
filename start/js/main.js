37c3330bb879ef95920ac479e6c06b8251e69bc2
My Name <myEmail@example.com>
2023-03-31 18:01:55 +0300

    100%

--- start/js/main.js
+++ start/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
     var mixer = mixitup('.portfolio__content');
+
+        $('.slider-blog__inner').slick({
+            dots: true,
+            prevArrow: '<button type="button" class="slick-prev"><img src="./images/arrPrev.svg" alt=""></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="./images/arrNext.svg" alt=""></button>',
+        });
+
 });
